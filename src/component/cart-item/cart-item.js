c37b3840d81ff4171625ec69b8aba9b876c8adc6
Waleed Zebary <100297062+waleedzebary@users.noreply.github.com>
2023-05-05 17:51:36 +0300

    Update cart-item.js

--- src/component/cart-item/cart-item.js
+++ src/component/cart-item/cart-item.js
@@ -7,7 +7,7 @@ const CartItem = ({ cartItem }) => {
         <div className='cart-item-container'>
             <img src={imageUrl} alt={`${name}`} />
             <div className='item-details'>
-                <span className='price'>{quantity} x {price}</span>
+                <span className='price'>{quantity} x {price}$</span>
             </div>
         </div>
     );
