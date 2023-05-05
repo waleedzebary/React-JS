6544981f433be20c4ae2d5a462b5e5d5a675ac23
Waleed Zebary <100297062+waleedzebary@users.noreply.github.com>
2023-05-05 17:55:15 +0300

    Update checkout-item.js

--- src/component/checkout-item/checkout-item.js
+++ src/component/checkout-item/checkout-item.js
@@ -2,39 +2,38 @@ import { useContext } from 'react';
 
 import { CartContext } from '../../contexts/cart-context';
 
-import './checkout-item.scss';
+import CheckoutItem from '../../component/checkout-item/checkout-item';
 
-const CheckoutItem = ({ cartItem }) => {
-  const { name, imageUrl, price, quantity } = cartItem;
+import './checkout.scss';
 
-  const { clearItemFromCart, addItemToCart, removeItemToCart } =
-    useContext(CartContext);
-
-  const clearItemHandler = () => clearItemFromCart(cartItem);
-  const addItemHandler = () => addItemToCart(cartItem);
-  const removeItemHandler = () => removeItemToCart(cartItem);
-
-  return (
-    <div className='checkout-item-container'>
-      <div className='image-container'>
-        <img src={imageUrl} alt={`${name}`} />
-      </div>
-      <span className='name'> {name} </span>
-      <span className='quantity'>
-        <div className='arrow' onClick={removeItemHandler}>
-          &#10094;
+const Checkout = () => {
+    const { cartItems, cartTotal } = useContext(CartContext);
+  
+    return (
+      <div className='checkout-container'>
+        <div className='checkout-header'>
+          <div className='header-block'>
+            <span>Product</span>
+          </div>
+          <div className='header-block'>
+            <span>Description</span>
+          </div>
+          <div className='header-block'>
+            <span>Quantity</span>
+          </div>
+          <div className='header-block'>
+            <span>Price</span>
+          </div>
+          <div className='header-block'>
+            <span>Remove</span>
+          </div>
         </div>
-        <span className='value'>{quantity}</span>
-        <div className='arrow' onClick={addItemHandler}>
-          &#10095;
-        </div>
-      </span>
-      <span className='price'> {price}</span>
-      <div className='remove-button' onClick={clearItemHandler}>
-        &#10005;
+        {cartItems.map((cartItem) => (
+          <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+        ))}
+        <div className='total'>TOTAL: {cartTotal}$</div>
       </div>
-    </div>
-  );
-};
-
-export default CheckoutItem;
+    );
+  };
+  
+  export default Checkout;
