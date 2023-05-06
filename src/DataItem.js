0838dd3f23e2b7e6ce677248c27913ef9416bb1e
Waleed Zebary <100297062+waleedzebary@users.noreply.github.com>
2023-05-06 17:45:17 +0300

    Update DataItem.js

--- src/DataItem.js
+++ src/DataItem.js
@@ -1,33 +1,63 @@
 export const dataItem = [
     {
-      id : 1,
-      title: "Hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png"
+      id: 1,
+      title: 'Audi',
+      imageUrl: require('./images/audi_r8.jpg')
     },
     {
       id: 2,
-      title: 'Jackets',
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"
+      title: 'Ferrari',
+      imageUrl: require('./images/ferrari_488_gtb.jpg')
     },
     {
       id: 3,
-      title: 'Sneakers',
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"
+      title: 'Mercedes Benz',
+      imageUrl: require('./images/mercedes_benz_s_class.jpg')
     },
     {
       id: 4,
-      title: 'Womens',
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png"
+      title: 'Toyota Camry',
+      imageUrl: require('./images/toyota_camry.jpg')
     },
     {
       id: 5,
-      title: 'Mens',
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png"
+      title: 'BMW',
+      imageUrl: require('./images/bmw_7_series.jpg')
     },
     {
       id: 6,
-      title: 'Shoes',
-      imageUrl: require('./image/shoes.jpg'),
-    }
+      title: 'Chevrolet Corvette',
+      imageUrl: require('./images/chevrolet_corvette.jpg')
+    },
+    {
+      id: 7,
+      title: 'Ford Mustang',
+      imageUrl: require('./images/ford_mustang.jpg')
+    },
+    {
+      id: 8,
+      title: 'Honda Civic',
+      imageUrl: require('./images/honda_civic.jpg')
+    },
+    {
+      id: 9,
+      title: 'Lamborghini',
+      imageUrl: require('./images/lamborghini_aventador.jpg')
+    },
+    {
+      id: 10,
+      title: 'Mclaren',
+      imageUrl: require('./images/mclaren_720s.jpg')
+    },
+    {
+      id: 11,
+      title: 'Porsche',
+      imageUrl: require('./images/porsche_911.jpg')
+    },
+    {
+      id: 12,
+      title: 'Tesla',
+      imageUrl: require('./images/tesla_model_s.jpg')
+    },
   ];
   
