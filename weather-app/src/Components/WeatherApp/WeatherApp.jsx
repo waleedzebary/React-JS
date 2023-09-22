3c12e89bee3ac88be29eab3a948709640ea20f48
waleed zebary <waleedzebary321@gmail.com>
2023-09-22 20:14:45 +0300

    starting the project

---
+++ weather-app/src/Components/WeatherApp/WeatherApp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './WeatherApp.css';
+
+
+const WeatherApp = () => {
+    return(
+        <div className="container">
+           
+        </div>
+    )
+}
+
+export default WeatherApp; 
